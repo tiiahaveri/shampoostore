6fec6617d0aab9067f859caa82b25f67b485c4a6
Tiia Haverinen <bgo844@myy.haaga-helia.fi>
2021-11-16 19:52:35 +0200

    system.properties file added

--- src/main/java/Shampoostore/domain/Product.java
+++ src/main/java/Shampoostore/domain/Product.java
@@ -9,7 +9,6 @@ import javax.persistence.ManyToOne;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 
